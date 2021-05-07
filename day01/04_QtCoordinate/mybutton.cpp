c75a8e82f6e3b373fdf490d8302a7459ce0844f3
忘言 <60057517+shiwangyan@users.noreply.github.com>
2021-05-07 17:15:28 +0800

    Add files via upload

---
+++ day01/04_QtCoordinate/mybutton.cpp
@@ -0,0 +1,13 @@
+#include "mybutton.h"
+#include <QtDebug>
+
+MyButton::MyButton(QWidget *parent) : QPushButton(parent)
+{
+
+}
+
+
+MyButton::~MyButton()
+{
+    qDebug()<<"按钮被析构";
+}
---
+++ day01/04_QtCoordinate/mybutton.h
@@ -0,0 +1,17 @@
+#ifndef MYBUTTON_H
+#define MYBUTTON_H
+
+#include <QPushButton>
+
+class MyButton : public QPushButton
+{
+    Q_OBJECT
+public:
+    explicit MyButton(QWidget *parent = nullptr);
+    ~MyButton();
+
+signals:
+
+};
+
+#endif // MYBUTTON_H
